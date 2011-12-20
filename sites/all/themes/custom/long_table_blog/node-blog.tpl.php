85d4a307dc4fca4abae8467bddab3a453c68ad4b
Richard Brandon <richard.brandon@gmail.com>
2011-12-20 14:40:19 -0500

    update the more link to dynamically display more

--- sites/all/themes/custom/long_table_blog/node-blog.tpl.php
+++ sites/all/themes/custom/long_table_blog/node-blog.tpl.php
@@ -28,7 +28,7 @@
       $teaser_more = node_teaser($node->body,NULL,1600);
       if(strlen($teaser_more) > strlen($content)){
       	$class_extra = (strlen($teaser_more) == strlen($node->body))?" complete":"";
-      	print '<div class="content-more'.$class_extra.'">'.$teaser_more.'</div>';
+      	print '<div class="content-more'.$class_extra.'" style="display:none;">'.$teaser_more.'</div>';
       }
     ?>
     <div class="submitted-by">submitted by <strong><?php print $name ?></strong>  <?php if ($submitted): ?>on <strong><?php print $submitted; ?></strong><?php endif; ?> <?php if ($terms): ?> <?php print t(' in ') . $terms; ?>  <?php endif; ?></div>
