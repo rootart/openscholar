69c715ecacd31e2db6fd58e48a92915d1e926b4f
Amitaibu <amitai@gizra.com>
2012-10-03 09:53:58 +0200

    Update OG-vocab.

--- modules/contrib/og_vocab/og_vocab.api.php
+++ modules/contrib/og_vocab/og_vocab.api.php
@@ -15,10 +15,15 @@
 /**
  * Allow modules to return TRUE if we are insiede an OG vocab admin
  * context.
+ *
+ * @return
+ *   If countext found array keyed by the group-type and the group ID.
  */
 function hook_og_vocab_is_admin_context() {
   $item = menu_get_item();
-  return $item['path'] == 'foo/admin';
+  if (strpos($item['path'], 'foo/admin') === 0){
+    return array('group_type' => 'node', 'gid' => 1);
+  }
 }
 
 /**
